81d2a9a3a73aabb1a42d44e2d7d409f3ffc17d84
Cecilia <ccchimento@gmail.com>
2024-07-05 22:10:57 -0600

    grid fill

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
-import './styles.css';
+import { useEffect, useState } from "react";
+
+import "./styles.css";
 
 export default function App() {
-  return <div>Hello World</div>;
+  const [activated, setActivated] = useState([]);
+
+  function handleClick(e) {
+    if (activated.length < 8) {
+      const id = e.target.id;
+      if (!activated.includes(id)) {
+        setActivated([...activated, id]);
+      }
+    }
+  }
+
+  const cellsDisplay = [];
+  for (let i = 0; i < 9; i++) {
+    if (i !== 4) {
+      cellsDisplay.push(
+        <Cell
+          key={i}
+          id={i}
+          handleClick={handleClick}
+          activated={activated.includes(i.toString())}
+        />
+      );
+    } else {
+      cellsDisplay.push(<span key={i} />);
+    }
+  }
+
+  return <div className="container">{cellsDisplay}</div>;
+}
+
+function Cell({ id, handleClick, activated }) {
+  return (
+    <div
+      className="cell"
+      id={id}
+      onClick={(e) => handleClick(e)}
+      style={{ backgroundColor: activated ? "darkseagreen" : null }}
+    />
+  );
 }
